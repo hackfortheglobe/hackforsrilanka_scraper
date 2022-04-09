cb10c58483d0502f4396dfd29ef0b37bfc746596
kenkun091 <kun.guo91@gmail.com>
2022-04-09 13:24:45 -0700

    update utils.py

--- utils.py
+++ utils.py
@@ -15,6 +15,7 @@ import json
 # sl_time = datetime.now(pytz.timezone('Asia/Colombo')).strftime('%Y-%m-%d')
 
 class power_cut_schedule():
+
     def __int__(self):
         # get Sri Lanka local time
         self.sl_time = datetime.now(pytz.timezone('Asia/Colombo')).strftime('%Y-%m-%d')
@@ -28,64 +29,68 @@ class power_cut_schedule():
         # look for google drive link, remove duplicates
         self.id = list(set([i for i in links if 'drive.google.com' in i]))
 
+    def get_confirm_token(self, response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+        return None
+
+    def convert_time(self, time_str):
+        time_str = self.sl_time + 'T' + time_str + ':00.000Z'
+        return time_str
+
+    def save_response_content(self):
+        CHUNK_SIZE = 32768
+
+        with open(self.destination, "wb") as f:
+            for chunk in self.response.iter_content(CHUNK_SIZE):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+
     def download_file_from_google_drive(self):
         URL = "https://docs.google.com/uc?export=download"
 
         session = requests.Session()
 
         response = session.get(URL, params={'id': self.id}, stream=True)
-        token = get_confirm_token(response)
+        token = self.get_confirm_token(response)
 
         if token:
-            params = {'id': id, 'confirm': token}
-            response = session.get(URL, params=params, stream=True)
+            params = {'id': self.id, 'confirm': token}
+            self.response = session.get(URL, params=params, stream=True)
 
-        save_response_content(response, self.destination)
+        self.save_response_content(self)
 
-    def get_confirm_token(response):
-        for key, value in response.cookies.items():
-            if key.startswith('download_warning'):
-                return value
+    def retrieve_tables(self):
+        self.file_id = self.id[0].split('/')[5]
+        self.download_file_from_google_drive(self)
+        self.tables = camelot.read_pdf('./assets/ceb_googledoc.pdf')
 
-    def convert_time(self, time_str):
-        time_str = self.sl_time+'T'+time_str+':00.000Z'
-        return time_str
+        # return self.tables
 
+    def process_tables(self):
+        dff = pd.DataFrame()
 
+        for ii in range(2):
 
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
+            df = self.tables[ii].df
 
-    return None
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
+            df = pd.DataFrame(data=df.iloc[1:].values, columns=df.iloc[0])
+            df['starting_period'] = df['Period'].apply(lambda x: x.split(' ')[0] if ' ' in x else x.split('-')[0])
+            df['ending_period'] = df['Period'].apply(lambda x: x.split(' ')[-1] if ' ' in x else x.split('-')[-1])
 
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
+            df['starting_period'] = df['starting_period'].apply(lambda x: x.replace('–', '')).apply(lambda x: self.convert_time(x))
+            df['ending_period'] = df['ending_period'].apply(lambda x: x.replace('–', '')).apply(lambda x: self.convert_time(x))
 
-def process_tables(tables):
-    dff = pd.DataFrame()
-
-    for ii in range(2):
-
-        df = tables[ii].df
-
-        df = pd.DataFrame(data=df.iloc[1:].values, columns=df.iloc[0])
-        df['starting_period'] = df['Period'].apply(lambda x: x.split(' ')[0] if ' ' in x else x.split('-')[0])
-        df['ending_period'] = df['Period'].apply(lambda x: x.split(' ')[-1] if ' ' in x else x.split('-')[-1])
-
-        df['starting_period'] = df['starting_period'].apply(lambda x: x.replace('–', '')).apply(lambda x: convert_time(x))
-        df['ending_period'] = df['ending_period'].apply(lambda x: x.replace('–', '')).apply(lambda x: convert_time(x))
-
-        for jj in range(len(df)):
-            eles = df['Schedule Group'][jj].replace(' ', '').split(',')
-            eles = ' '.join(eles).split()
-            for ele in eles:
-                dff = dff.append(pd.DataFrame(data={'group_name': ele, 'starting_period': [df.iloc[jj]['starting_period']],
-                                                    'ending_period': [df.iloc[jj]['ending_period']],
-                                                    'unique_id': str(uuid.uuid1())}))
+            for jj in range(len(df)):
+                eles = df['Schedule Group'][jj].replace(' ', '').split(',')
+                eles = ' '.join(eles).split()
+                for ele in eles:
+                    dff = dff.append(pd.DataFrame(data={'group_name': ele, 'starting_period': [df.iloc[jj]['starting_period']],
+                                                        'ending_period': [df.iloc[jj]['ending_period']],
+                                                        }))
         return dff
+
+    def output_json(self):
+        json_out = self.process_tables(self).reset_index(drop=True).to_json(orient='records')
+        self.dict_obj = {"schedules": json.loads(json_out)}
