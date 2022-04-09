9e86575d7eabb0d9c9f4f08bffebd8b79c80e599
kenkun091 <kun.guo91@gmail.com>
2022-04-09 13:49:37 -0700

    update utils.py

--- utils.py
+++ utils.py
@@ -5,40 +5,50 @@ import pandas as pd
 
 from datetime import datetime
 import pytz
-import uuid
 import json
 
-# request, bypass certificate check
-
-
-# get Sri Lanka local time
-# sl_time = datetime.now(pytz.timezone('Asia/Colombo')).strftime('%Y-%m-%d')
-
 class power_cut_schedule():
-
     def __int__(self):
         # get Sri Lanka local time
         self.sl_time = datetime.now(pytz.timezone('Asia/Colombo')).strftime('%Y-%m-%d')
         self.destination = './assets/ceb_googledoc'
 
     def retrieve_url(self):
+        # request, bypass certificate check
         req = requests.get('https://ceb.lk', verify=False)
         webpage = html.fromstring(req.content)
         links = webpage.xpath('//a/@href')
 
-        # look for google drive link, remove duplicates
-        self.id = list(set([i for i in links if 'drive.google.com' in i]))
+        # look for google drive link
+        url_link = [i for i in links if 'drive.google.com' in i]
 
-    def get_confirm_token(self, response):
-        for key, value in response.cookies.items():
-            if key.startswith('download_warning'):
-                return value
-        return None
+        # remove duplicates
+        url_link = list(set(url_link))
+
+        self.id = url_link[0].split('/')[5]
 
     def convert_time(self, time_str):
         time_str = self.sl_time + 'T' + time_str + ':00.000Z'
         return time_str
 
+    def download_file_from_google_drive(self):
+        URL = "https://docs.google.com/uc?export=download"
+
+        session = requests.Session()
+
+        self.response = session.get(URL, params={'id': self.id}, stream=True)
+        token = self.get_confirm_token(self)
+
+        if token:
+            params = {'id': self.id, 'confirm': token}
+            self.response = session.get(URL, params=params, stream=True)
+
+        self.save_response_content(self)
+    def get_confirm_token(self):
+        for key, value in self.response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+        return None
     def save_response_content(self):
         CHUNK_SIZE = 32768
 
@@ -47,26 +57,9 @@ class power_cut_schedule():
                 if chunk:  # filter out keep-alive new chunks
                     f.write(chunk)
 
-    def download_file_from_google_drive(self):
-        URL = "https://docs.google.com/uc?export=download"
-
-        session = requests.Session()
-
-        response = session.get(URL, params={'id': self.id}, stream=True)
-        token = self.get_confirm_token(response)
-
-        if token:
-            params = {'id': self.id, 'confirm': token}
-            self.response = session.get(URL, params=params, stream=True)
-
-        self.save_response_content(self)
-
     def retrieve_tables(self):
-        self.file_id = self.id[0].split('/')[5]
         self.download_file_from_google_drive(self)
-        self.tables = camelot.read_pdf('./assets/ceb_googledoc.pdf')
-
-        # return self.tables
+        self.tables = camelot.read_pdf(self.destination)
 
     def process_tables(self):
         dff = pd.DataFrame()
@@ -79,8 +72,8 @@ class power_cut_schedule():
             df['starting_period'] = df['Period'].apply(lambda x: x.split(' ')[0] if ' ' in x else x.split('-')[0])
             df['ending_period'] = df['Period'].apply(lambda x: x.split(' ')[-1] if ' ' in x else x.split('-')[-1])
 
-            df['starting_period'] = df['starting_period'].apply(lambda x: x.replace('–', '')).apply(lambda x: self.convert_time(x))
-            df['ending_period'] = df['ending_period'].apply(lambda x: x.replace('–', '')).apply(lambda x: self.convert_time(x))
+            df['starting_period'] = df['starting_period'].apply(lambda x: x.replace('–', '')).apply(lambda x: self.convert_time(self,x))
+            df['ending_period'] = df['ending_period'].apply(lambda x: x.replace('–', '')).apply(lambda x: self.convert_time(self, x))
 
             for jj in range(len(df)):
                 eles = df['Schedule Group'][jj].replace(' ', '').split(',')
