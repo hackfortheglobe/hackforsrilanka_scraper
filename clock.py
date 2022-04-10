59ec0fc29cadd9ade9ccf6210b543b3a853db353
Jinal Shah <jinalshah2007@gmail.com>
2022-04-09 17:17:04 -0700

    Adding update as well

--- clock.py
+++ clock.py
@@ -2,6 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
 
 sched = BlockingScheduler()
+subprocess.run(['apt-get update'])
 subprocess.run(['apt-get','install','ffmpeg libsm6 libxext6  -y'])
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
