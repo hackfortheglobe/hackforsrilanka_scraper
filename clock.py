44bd0fa50dfef2189bd23c86eacf5b534651b4c8
Jinal Shah <jinalshah2007@gmail.com>
2022-04-09 17:13:38 -0700

    testing out to see if this works out to install the missing packages.

--- clock.py
+++ clock.py
@@ -2,10 +2,10 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
 
 sched = BlockingScheduler()
-
+subprocess.run(['apt-get','install','ffmpeg libsm6 libxext6  -y'])
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
-
+    
     subprocess.run(["python", "main.py"])
 
 sched.start()
