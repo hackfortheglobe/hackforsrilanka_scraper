de2fddde00d79e81eb3af792b828275adab5e73b
Rafael Acosta <rafaco@gmail.com>
2022-05-06 18:06:07 -0700

    Replace group_name by group at json output, as expected by api

--- testExtractionBatch.py
+++ testExtractionBatch.py
@@ -97,7 +97,7 @@ for pdfFileName in pdfFileNames:
     csvPath = os.path.join(base,'outputs', pdfFileName + "_schedules" + '.csv')
     data = []
     for schedule in json_schedules["schedules"]:
-        row = {"group_name": schedule["group_name"], "starting_period": schedule["starting_period"], "endinging_period": schedule["starting_period"]}
+        row = {"group": schedule["group"], "starting_period": schedule["starting_period"], "endinging_period": schedule["starting_period"]}
         data.append(row)
     df = pd.DataFrame(data)
     df.to_csv (csvPath, index = None)
